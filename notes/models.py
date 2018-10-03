ad55c86960745fff83dde301b1f4d73774a02eca
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-10-03 17:14:39 -0400

    Add __str__(), relax some constraints

--- notes/models.py
+++ notes/models.py
@@ -3,15 +3,18 @@ from django.db import models
 # Create your models here.
 class TextNote(models.Model):
     title = models.CharField(max_length=80, help_text='This is a short descriptive title for the note.')
-    text = models.TextField(help_text='This is the contents of the note')
+    text = models.TextField(help_text='This is the contents of the note', blank=True)
     createdOn = models.DateTimeField(auto_now_add=True)
     lastUpdated = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey('Category', models.PROTECT)
+    category = models.ForeignKey('Category', models.PROTECT, null=True)
     class Meta:
         indexes = [
             models.Index(fields=['category'])
         ]
         unique_together = ('title', 'createdOn')
+
+    def __str__(self):
+        return "Text Note: {}".format(self.title)
 
 
 class Category(models.Model):
@@ -21,4 +24,7 @@ class Category(models.Model):
         indexes = [
             models.Index(fields=['parent'])
         ]
+
+    def __str__(self):
+        return self.name
 
