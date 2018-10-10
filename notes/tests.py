0959e5d6041f4afe39095f8736b76d27fa6a42b3
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2018-10-10 15:07:08 -0400

    Fix test

--- notes/tests.py
+++ notes/tests.py
@@ -7,10 +7,10 @@ from .models import TextNote
 
 class TextNoteModelTests(TransactionTestCase):
 
-    def can_create_note(self):
+    def test_can_create_note(self):
         time = timezone.now()
         fred = TextNote(title='This is test note', text='Not all those who wander are lost')
         fred.save()
         delta = fred.createdOn - time
-        self.assertGreaterEqual(timezone.timedelta(0), delta, 'created date is in the future???')
-        self.assertLessEqual(timezone.timedelta(seconds=2), delta, 'created date is too far in past')
+        self.assertLessEqual(timezone.timedelta(0), delta, 'created date is in the future???')
+        self.assertGreaterEqual(timezone.timedelta(seconds=2), delta, 'created date is too far in past')
